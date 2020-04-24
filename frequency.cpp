20a35845d0622bc529f8b7ee641b7d5f65e8b203
camillezajac <19757948+camillezajac@users.noreply.github.com>
2020-04-24 02:18:23 -0700

    Update frequency.cpp

--- frequency.cpp
+++ frequency.cpp
@@ -8,8 +8,7 @@ int chartonum(char cha){
     return ((int)cha) - 97; // ASCII a = 97
 }
 
-int main(int argc, char *argv[])
-{
+int main(int argc, char *argv[]) {
 	fstream inFile;
 	char oneChar;
 	char ASCII[128] = {0}; //127 characters of the ASCII +1 
@@ -17,49 +16,43 @@ int main(int argc, char *argv[])
 	char code;
 	double total;
 	
-	for(int ASCII = 0; ASCII < 128; ASCII++) //Loop through all the ASCII characters to get the frequency of each
-	{
+	//Loop through ASCII characters to get the frequency of each
+	for(int ASCII = 0; ASCII < 128; ASCII++) {
 		frequency[ASCII] = 0;
 	}
 
 	inFile.open("cipher.txt", ios::in); //Reads file
-
-	if(inFile.fail()) //Check for error opening the file
-	{
+	
+	//Check for error opening the file
+	if(inFile.fail()) {
 		cout << "Error: File not found!" << endl << endl; 
-	}
-	else
-	{
+	} else {
 		oneChar = inFile.get();
 
-		while(inFile.eof() == false) //End of file
-		{
-			if(oneChar != ' ')
-			{
-				frequency[oneChar]++; //When not empty character
+		//End of file
+		while(inFile.eof() == false) {
+			//When not empty character
+			if(oneChar != ' ') {
+				frequency[oneChar]++; 
 				total++;
-
 			}
 			oneChar = inFile.get();
-			
 		}
 	}
-
+	
 	inFile.close();
-
-	cout << "The Frequency of the capital letters are: " << endl;
-	for(char caps = 'A'; caps <= 'Z'; caps++)
-	{
-
+	
+	cout << "Frequency of uppercase letters: " << endl;
+	
+	for(char caps = 'A'; caps <= 'Z'; caps++) {
 		cout << "Letter " << caps << " is " << setw(4) << frequency[caps] << " times." << endl;
 		double frequencyDoub = (double) frequency[caps];
 		double percent = (frequencyDoub/total)*100;
 		cout << " Percent of total: " << percent << endl;			
-
 	}
 	cout << "Total: " << total << endl;
 
-	// cout << "The Frequency of the lower case letters are: " << endl;
+	// cout << "Frequency of the lower case letters: " << endl;
 	// for(char lower = 'a'; lower <= 'z'; lower++)
 	// {
 	// 	cout << "Letter " << lower << " is " << setw(4) << frequency[lower] << " times." << endl;
